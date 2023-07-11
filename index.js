6b0cd0eb1027eb5b7f5aec196e8177f0c5d9d326
Yuliana Velez <velezrodriguez2001@gmail.com>
2023-07-10 19:50:55 -0500

    Added styles

--- index.js
+++ index.js
@@ -5,6 +5,39 @@ const booksContainer = document.querySelector (".container")
 const booksTitle = document.querySelector("#booksTitle")
 const booksAuthors = document.querySelector("#booksAuthors")
 const addButton = document.querySelector(".addButton")
+const navList = document.querySelector("#navList")
+const navAdd = document.querySelector("#navAdd")
+const navContact = document.querySelector("#navContact")
+const listSection = document.querySelector("#listSection")
+const addSection = document.querySelector("#addSection")
+const contactSection = document.querySelector("#contactSection")
+const datetime = document.getElementById('datetime');
+
+setInterval(() => {
+  const now = new Date();
+  const options = { year: 'numeric', month: 'long', day: 'numeric' };
+  const date = now.toLocaleDateString('en-US', options);
+  const time = now.toLocaleTimeString();
+  datetime.innerHTML = `${date} ${time}`;
+}, 1000);
+
+navList.addEventListener('click', () => {
+  listSection.classList.remove('hidden');
+  addSection.classList.add('hidden');
+  contactSection.classList.add('hidden');
+});  
+
+navAdd.addEventListener('click', () => {
+  listSection.classList.add('hidden');
+  addSection.classList.remove('hidden');
+  contactSection.classList.add('hidden');
+});  
+
+navContact.addEventListener('click', () => {
+  listSection.classList.add('hidden');
+  addSection.classList.add('hidden');
+  contactSection.classList.remove('hidden');
+});  
 
 let booksStorage = new BookStorage(booksContainer)
 
